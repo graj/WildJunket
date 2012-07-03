99f88d2ebf406ccfd5234b0dee79ff510f2a44f1
David Garcia <dangior@gmail.com>
2012-07-04 01:48:47 +0300

    Added tabbar views and icons

---
+++ WildJunket/PhotosViewController.m
@@ -0,0 +1,34 @@
+//
+//  SecondViewController.m
+//  WildJunket
+//
+//  Created by David García Fernández on 16/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PhotosViewController.h"
+
+@interface PhotosViewController ()
+
+@end
+
+@implementation PhotosViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
---
+++ WildJunket/RSSViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  WildJunket
+//
+//  Created by David García Fernández on 16/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSSViewController : UIViewController
+
+@end
