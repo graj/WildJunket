616bcbfa64deed3b2fba9b0cc45dfe1e6770a214
David Garcia <dangior@gmail.com>
2012-07-04 20:58:52 +0300

    Added nav controller and classes

---
+++ WildJunket/RSSNavViewController.h
@@ -0,0 +1,13 @@
+//
+//  RSSNavViewController.h
+//  WildJunket
+//
+//  Created by David García Fernández on 04/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSSNavViewController : UINavigationController
+
+@end
